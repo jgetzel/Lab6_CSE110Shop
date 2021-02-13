c7ce2a9b0073c8ed29683200e211a43e5e69fc6d
jgetzel <60719706+jgetzel@users.noreply.github.com>
2021-02-13 02:47:08 -0800

    Fixed!

--- scripts/script.js
+++ scripts/script.js
@@ -9,48 +9,58 @@ window.addEventListener('DOMContentLoaded', () => {
     if(!storage.getItem('fetch')){
       fetch('https://fakestoreapi.com/products')
         .then(data => data.json())
-        .then(success => storage.setItem('fetch', JSON.stringify(success)));
+        .then(success => onFetched(success));
     }
-    
-    let jsonData = JSON.parse(storage.getItem('fetch'));
-
-    for (let i = 0; i < jsonData.length; i++){
-      let product = document.createElement('product-item');
-      product.shadowRoot.getElementById('img').setAttribute('src', jsonData[i].image);
-      product.shadowRoot.getElementById('title').textContent = jsonData[i].title;
-      product.shadowRoot.getElementById('price').textContent = jsonData[i].price;
-      product.shadowRoot.getElementById('button').addEventListener('click', function(){
-        let button = product.shadowRoot.getElementById('button');
-        let index = i;
-        if(button.textContent == "Add to Cart"){
-          alert("Added To Cart!");
-          button.textContent = "Remove from Cart";
-          cartCount.textContent = Number(cartCount.textContent) + 1;
-          storage.setItem(`item-${index}`,'added');
-        }
-        else{
-          button.textContent = "Add to Cart";
-          cartCount.textContent = Number(cartCount.textContent) - 1;
-          storage.setItem(`item-${index}`,'not-added');
-        }
-      });
-
-      let item = storage.getItem(`item-${i}`);
-      if(item == 'added'){
-        product.shadowRoot.getElementById('button').textContent = "Remove from Cart";
-        cartCount.textContent = Number(cartCount.textContent) + 1;
-      }
-      else if(item == 'not-added'){
-        product.shadowRoot.getElementById('button').textContent = "Add to Cart";
-      }
-
-      /*
-      product.setAttribute('text', jsonData[i].title);
-      product.setAttribute('price', jsonData[i].price);
-      product.setAttribute('img', jsonData[i].image);
-      */
-
-      container.appendChild(product);
+    else{
+      initialize();
     }
 });
 
+function onFetched(success){
+  storage.setItem('fetch', JSON.stringify(success));
+
+  initialize();
+}
+
+function initialize(){
+  let jsonData = JSON.parse(storage.getItem('fetch'));
+
+  for (let i = 0; i < jsonData.length; i++){
+    let product = document.createElement('product-item');
+    product.shadowRoot.getElementById('img').setAttribute('src', jsonData[i].image);
+    product.shadowRoot.getElementById('title').textContent = jsonData[i].title;
+    product.shadowRoot.getElementById('price').textContent = jsonData[i].price;
+    product.shadowRoot.getElementById('button').addEventListener('click', function(){
+      let button = product.shadowRoot.getElementById('button');
+      let index = i;
+      if(button.textContent == "Add to Cart"){
+        alert("Added To Cart!");
+        button.textContent = "Remove from Cart";
+        cartCount.textContent = Number(cartCount.textContent) + 1;
+        storage.setItem(`item-${index}`,'added');
+      }
+      else{
+        button.textContent = "Add to Cart";
+        cartCount.textContent = Number(cartCount.textContent) - 1;
+        storage.setItem(`item-${index}`,'not-added');
+      }
+    });
+
+    let item = storage.getItem(`item-${i}`);
+    if(item == 'added'){
+      product.shadowRoot.getElementById('button').textContent = "Remove from Cart";
+      cartCount.textContent = Number(cartCount.textContent) + 1;
+    }
+    else if(item == 'not-added'){
+      product.shadowRoot.getElementById('button').textContent = "Add to Cart";
+    }
+
+    /*
+    product.setAttribute('text', jsonData[i].title);
+    product.setAttribute('price', jsonData[i].price);
+    product.setAttribute('img', jsonData[i].image);
+    */
+
+    container.appendChild(product);
+  }
+}
