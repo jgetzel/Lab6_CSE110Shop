2b0a8aefde286dcb16c8edf74129e40c97616107
jgetzel <60719706+jgetzel@users.noreply.github.com>
2021-02-13 02:30:53 -0800

    Done!

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,112 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor(){
+    super();
+
+    const shadowRoot = this.attachShadow({mode: 'open'});
+
+    shadowRoot.innerHTML = `
+
+    <style>
+      
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    </style>
+    <li class="product">
+      <img id="img" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200px>
+      <p id="title" class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+      <p id="price" class="price">$109.95</p>
+      <button id="button">Add to Cart</button>
+    </li>
+    `;
+
+    /*
+    const list = document.createElement('li');
+    list.class = "product";
+
+    let imgURL = this.hasAttribute('img') ? this.getAttribute('img') : "";
+    
+    let img = shadowRoot.getElementById('img');
+    img.src = imgURL;
+    img.alt = this.getAttribute('text');
+    img.width = "200px";  
+    list.appendChild(img);
+    
+    const title = list.appendChild(document.createElement('p'));
+    title.class = 'title';
+    title.textContent = this.getAttribute('text');
+
+    const price = list.appendChild(document.createElement('p'));
+    price.class = 'price';
+    price.textContent = this.getAttribute('price');
+
+    const button = list.appendChild(document.createElement('button'));
+    button.setAttribute("onclick", "alert('Added To Cart!')");
+    */
+    //this.shadowRoot.append(list);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,56 @@
 // Script.js
+let storage = window.localStorage
+
+let container = document.getElementById("product-list");
+let cartCount = document.getElementById("cart-count");
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+    if(!storage.getItem('fetch')){
+      fetch('https://fakestoreapi.com/products')
+        .then(data => data.json())
+        .then(success => storage.setItem('fetch', JSON.stringify(success)));
+    }
+    
+    let jsonData = JSON.parse(storage.getItem('fetch'));
+
+    for (let i = 0; i < jsonData.length; i++){
+      let product = document.createElement('product-item');
+      product.shadowRoot.getElementById('img').setAttribute('src', jsonData[i].image);
+      product.shadowRoot.getElementById('title').textContent = jsonData[i].title;
+      product.shadowRoot.getElementById('price').textContent = jsonData[i].price;
+      product.shadowRoot.getElementById('button').addEventListener('click', function(){
+        let button = product.shadowRoot.getElementById('button');
+        let index = i;
+        if(button.textContent == "Add to Cart"){
+          alert("Added To Cart!");
+          button.textContent = "Remove from Cart";
+          cartCount.textContent = Number(cartCount.textContent) + 1;
+          storage.setItem(`item-${index}`,'added');
+        }
+        else{
+          button.textContent = "Add to Cart";
+          cartCount.textContent = Number(cartCount.textContent) - 1;
+          storage.setItem(`item-${index}`,'not-added');
+        }
+      });
+
+      let item = storage.getItem(`item-${i}`);
+      if(item == 'added'){
+        product.shadowRoot.getElementById('button').textContent = "Remove from Cart";
+        cartCount.textContent = Number(cartCount.textContent) + 1;
+      }
+      else if(item == 'not-added'){
+        product.shadowRoot.getElementById('button').textContent = "Add to Cart";
+      }
+
+      /*
+      product.setAttribute('text', jsonData[i].title);
+      product.setAttribute('price', jsonData[i].price);
+      product.setAttribute('img', jsonData[i].image);
+      */
+
+      container.appendChild(product);
+    }
 });
+
